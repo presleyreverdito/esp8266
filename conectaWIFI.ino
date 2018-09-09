5bba61e23cbcf80ebd721deb4ec91ca7491793eb
Presley Demuner Reverdito <presleyreverdito@gmail.com>
2018-09-09 13:37:45 -0400

    Algoritmo capaz de conectar o ESP8266 na rede wifi

---
+++ conectaWIFI.ino
@@ -0,0 +1,22 @@
+// Import required libraries
+#include <ESP8266WiFi.h>
+// WiFi parameters
+const char* ssid = "Identificando...";
+const char* password = "campestre21";
+void setup(void)
+{
+// Start Serial
+Serial.begin(115200);
+// Connect to WiFi
+WiFi.begin(ssid, password);
+while (WiFi.status() != WL_CONNECTED) {
+delay(500);
+Serial.print(".");
+}
+Serial.println("");
+Serial.println("WiFi connected");
+// Print the IP address
+Serial.println(WiFi.localIP());
+}
+void loop() {
+}
